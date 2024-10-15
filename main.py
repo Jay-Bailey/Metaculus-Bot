17e285d8a32fbda0b03b5090d8b0c7974779d8ef
Jay Bailey <jaybaileycs@gmail.com>
2024-10-15 15:54:41 +0100

    String

--- main.py
+++ main.py
@@ -298,7 +298,7 @@ async def call_ask_news(query, summariser_fn=call_claude):
     
     graph = ask.news.search_news(query=query, strategy='latest news', sentiment='neutral', diversify_sources=True, method='nl', categories=category_list, return_type="string")
     
-    summary, _ = await call_claude("You are an assistant to a superforecaster. The superforecaster will give you a question they intend to forecast on. To be a great assistant, you generate a concise but detailed rundown of the most relevant news, including if the question would resolve Yes or No based on current information. You do not produce forecasts yourself. The relevant news you have found is: " + graph)
+    summary, _ = await call_claude("You are an assistant to a superforecaster. The superforecaster will give you a question they intend to forecast on. To be a great assistant, you generate a concise but detailed rundown of the most relevant news, including if the question would resolve Yes or No based on current information. You do not produce forecasts yourself. The relevant news you have found is: " + graph.as_string)
 
     if DEBUG_MODE:
         logger.info(f"News summary: {summary}")
